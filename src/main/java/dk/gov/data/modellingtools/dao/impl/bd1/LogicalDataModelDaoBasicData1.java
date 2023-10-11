1c618ac5ff7ba16570a84e483d43ac274cca2172
Heidi Vanparys <hevan@sdfi.dk>
2023-10-11 15:31:20 +0200

    Update how the name and the version of a logical data model are set
    Many models do have their tagged values set correctly, so we cannot
    rely on that when exporting models. Therefore, use the UML package name
    as the name of the model, and check whether perhaps the version is set
    on the package if it is not set in a tagged value.

--- src/main/java/dk/gov/data/modellingtools/dao/impl/bd1/LogicalDataModelDaoBasicData1.java
+++ src/main/java/dk/gov/data/modellingtools/dao/impl/bd1/LogicalDataModelDaoBasicData1.java
@@ -4,17 +4,23 @@ import dk.gov.data.modellingtools.constants.BasicData1Constants;
 import dk.gov.data.modellingtools.dao.LogicalDataModelDao;
 import dk.gov.data.modellingtools.dao.impl.AbstractLogicalDataModelDao;
 import dk.gov.data.modellingtools.ea.EnterpriseArchitectWrapper;
+import dk.gov.data.modellingtools.ea.utils.EaModelUtils;
 import dk.gov.data.modellingtools.exception.ModellingToolsException;
 import dk.gov.data.modellingtools.model.LogicalDataModel;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Collection;
 import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sparx.Package;
 
 /**
  * Implementation of {@link LogicalDataModelDao} for the Basic Data modelling rules version 1.
  */
 public class LogicalDataModelDaoBasicData1 extends AbstractLogicalDataModelDao {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(LogicalDataModelDaoBasicData1.class);
 
   @SuppressFBWarnings("EI_EXPOSE_REP")
   public LogicalDataModelDaoBasicData1(EnterpriseArchitectWrapper eaWrapper) {
@@ -31,7 +37,17 @@ public class LogicalDataModelDaoBasicData1 extends AbstractLogicalDataModelDao {
       Map<String, String> taggedValues) {
     LogicalDataModel logicalDataModel = new LogicalDataModel();
     logicalDataModel.setName(umlPackage.GetName());
-    logicalDataModel.setVersion(taggedValues.get(BasicData1Constants.TAG_VERSION));
+    String versionFromTag = taggedValues.get(BasicData1Constants.TAG_VERSION);
+    String versionFromModel = umlPackage.GetVersion();
+    String version;
+    if (StringUtils.isBlank(versionFromTag)) {
+      LOGGER.warn("{} does not have tag {} set, taking the version set on the package itself",
+          EaModelUtils.toString(umlPackage), BasicData1Constants.TAG_VERSION);
+      version = versionFromModel;
+    } else {
+      version = versionFromTag;
+    }
+    logicalDataModel.setVersion(version);
     return logicalDataModel;
   }
 
--- src/main/java/dk/gov/data/modellingtools/dao/impl/bd2/LogicalDataModelDaoBasicData2.java
+++ src/main/java/dk/gov/data/modellingtools/dao/impl/bd2/LogicalDataModelDaoBasicData2.java
@@ -4,16 +4,22 @@ import dk.gov.data.modellingtools.constants.BasicData2Constants;
 import dk.gov.data.modellingtools.dao.LogicalDataModelDao;
 import dk.gov.data.modellingtools.dao.impl.AbstractLogicalDataModelDao;
 import dk.gov.data.modellingtools.ea.EnterpriseArchitectWrapper;
+import dk.gov.data.modellingtools.ea.utils.EaModelUtils;
 import dk.gov.data.modellingtools.exception.ModellingToolsException;
 import dk.gov.data.modellingtools.model.LogicalDataModel;
 import java.util.Collection;
 import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sparx.Package;
 
 /**
  * Implementation of {@link LogicalDataModelDao} for the Basic Data modelling rules v2.
  */
 public class LogicalDataModelDaoBasicData2 extends AbstractLogicalDataModelDao {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(LogicalDataModelDaoBasicData2.class);
 
   public LogicalDataModelDaoBasicData2(EnterpriseArchitectWrapper eaWrapper) {
     super(eaWrapper);
@@ -28,8 +34,18 @@ public class LogicalDataModelDaoBasicData2 extends AbstractLogicalDataModelDao {
   protected LogicalDataModel createLogicalDataModel(Package umlPackage,
       Map<String, String> taggedValues) {
     LogicalDataModel logicalDataModel = new LogicalDataModel();
-    logicalDataModel.setName(taggedValues.get(BasicData2Constants.TAG_TITLE_DA));
-    logicalDataModel.setVersion(taggedValues.get(BasicData2Constants.TAG_VERSION));
+    logicalDataModel.setName(umlPackage.GetName());
+    String versionFromTag = taggedValues.get(BasicData2Constants.TAG_VERSION);
+    String versionFromModel = umlPackage.GetVersion();
+    String version;
+    if (StringUtils.isBlank(versionFromTag)) {
+      LOGGER.warn("{} does not have tag {} set, taking the version set on the package itself",
+          EaModelUtils.toString(umlPackage), BasicData2Constants.TAG_VERSION);
+      version = versionFromModel;
+    } else {
+      version = versionFromTag;
+    }
+    logicalDataModel.setVersion(version);
     return logicalDataModel;
   }
 
